bb1d54b07c69517ff298673fe0908ca7ac1e0120
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2022-01-06 02:15:04 +0530

    Added Closing #2

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <div className="App__header">
+
+        <img className="App__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="">
+
+        </img>
+
+      </div>
+
+      <h1>Hi from instagram</h1>
+
+
     </div>
   );
 }
